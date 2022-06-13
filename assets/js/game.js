90f919efe7f734011083699495b7fa2f52e97f59
21xa <104792225+21xa@users.noreply.github.com>
2022-06-13 13:53:02 -0500

    Added starter code

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+// this creates a function named "fight"
+window.prompt("What is your robot's name?");
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  fight();
+
+console.log("here")
